76c885b97a4b5245b3ba1ed385bbdf3d836c7626
Graham Pople <grahampople@gmail.com>
2023-03-05 11:17:25 +0000

    Iterating through book

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,14 +8,14 @@ object Main extends App {
   // Implementing map for List[A]
   def map[A, B](in: List[A], mapper: A => B): List[B] = in match {
     case x :: Nil => List(mapper(x))
-    case x :: xs => mapper(x) +: map(xs, mapper)
+    case x :: xs  => mapper(x) +: map(xs, mapper)
   }
 
   // tailrec version
   @tailrec
   def map[A, B](in: List[A], acc: List[B], mapper: A => B): List[B] = in match {
     case x :: Nil => mapper(x) +: acc
-    case x :: xs => map(xs, mapper(x) +: acc, mapper)
+    case x :: xs  => map(xs, mapper(x) +: acc, mapper)
   }
 
   // Imperative version
@@ -28,7 +28,7 @@ object Main extends App {
   // Implementing filter for List[A]
   def filter[A](in: List[A], f: A => Boolean): List[A] = in match {
     case x :: Nil => if (f(x)) List(x) else List()
-    case x :: xs => if (f(x)) x +: filter(xs, f) else filter(xs, f)
+    case x :: xs  => if (f(x)) x +: filter(xs, f) else filter(xs, f)
   }
 
   // Call-by-name.
@@ -36,9 +36,9 @@ object Main extends App {
   // The cool thing here is that we only need to care about toProfile's return type.  The rest
   // of its type can be arbritarily complex.  E.g. this lets us generally test any block of code.
   def profile[A](toProfile: => A): (A, Long) = {
-    val start = System.nanoTime()
+    val start     = System.nanoTime()
     val result: A = toProfile
-    val end = System.nanoTime()
+    val end       = System.nanoTime()
     (result, start - end)
   }
 
@@ -55,7 +55,6 @@ object Main extends App {
   myAssert(5 > 3)
   // Without call-by-name, would have to write
   // myAssert(() => 5 > 3)
-
 
   // Using multiple parameter groups to e.g. implement my own while loop.
   @tailrec
@@ -76,11 +75,9 @@ object Main extends App {
   def needsImplicit(normalArg: Int)(implicit implictArg: Int): Int =
     normalArg * implictArg
 
-
   needsImplicit(1)(2)
   implicit val someInt = 42
   needsImplicit(1)
-
 
   // Currying.
   // A function that takes multiple args, can be translated into a series of function calls
@@ -104,10 +101,9 @@ object Main extends App {
 
   // Currying lets us do that more generically:
   val divMe = wrap("<div>")(_: String)("/div")
-  val pMe = wrap("<p>")(_: String)("/p")
+  val pMe   = wrap("<p>")(_: String)("/p")
 
   val someHtml2 = divMe("hello world")
-
 
   // Can also do partially applied functions without multiple parameter groups:
   def wrapTwo(prefix: String, body: String, suffix: String) =
@@ -115,7 +111,6 @@ object Main extends App {
 
   val wrapWithDiv = wrapTwo("<div>", _: String, "</div>")
   wrapWithDiv("hello world")
-
 
   // Advanced stuff:  .curried lets us curry a method.
   def add(x: Int, y: Int) = x + y
@@ -125,41 +120,73 @@ object Main extends App {
   // Except we can't do that, we have to do Eta expansion first to turn the add method into an
   // add function.
   val addAsFunction = add _
-  val addCurried = addAsFunction.curried
+  val addCurried    = addAsFunction.curried
   addCurried(1)(2)
 
   // This has basically produced
   // def addCurried(x: Int)(y: Int) = x + y
 
 
+  // Partial functions (not in the book) are different.
+  // They represent applying some only cases in a pattern match.
+
+  // Complete pattern match:
+  val donut = "Glazed Donut"
+  val tasteLevel = donut match {
+    case "Glazed Donut" | "Strawberry Donut" => "Very tasty"
+    case "Plain Donut" => "Tasty"
+    case _ => "Tasty"
+  }
+
+  // Part of the match, as a PartialFunction:
+  val isVeryTasty: PartialFunction[String, String] = {
+    case "Glazed Donut" | "Strawberry Donut" => "Very Tasty"
+  }
+
+  // Will get MatchError if this doesn't match
+  val partialFuncResult: String = isVeryTasty(donut)
+
+  val unknownTaste: PartialFunction[String, String] = {
+    case donut: _ => s"Unknown taste for donut = $donut"
+  }
+
+  val willMatchAll: PartialFunction[String, String] = isVeryTasty orElse unknownTaste
+
+  val partialFuncResult2: String = willMatchAll(donut)
+
+
+  // .collect() - also not in book, but very useful
+  // Takes a PartialFunction.
+  // Acts as both filter and map.  Only the results the PartialFunction matches will be returned.
+
+
   // Making lists
   List() == Nil
   1 :: 2 :: 3 :: Nil
 
-
   // Practicing recursion
   def reverseList[A](in: List[A]): List[A] = in match {
-    case Nil => Nil
+    case Nil     => Nil
     case x :: xs => reverseList(xs) :+ x
   }
 
   def reverseString(in: String): String = in.toList match {
     case x if x.isEmpty => new String()
-    case x :: xs => reverseString(xs.toString) + x
+    case x :: xs        => reverseString(xs.toString) + x
   }
 
   // Simple recursive sum
   // Note it's not tail recursive despite sum() being the last thing in there.
   // This is because really that last line is `val s = sum(tail); val result = head + s; return result;`
   def sum(xs: List[Int]): Int = xs match {
-    case Nil => 0
+    case Nil          => 0
     case head :: tail => head + sum(tail)
   }
 
   // Can make tail recursive by changing to an accumulator
   @tailrec
   def sum(acc: Int, xs: List[Int]): Int = xs match {
-    case Nil => 0
+    case Nil          => 0
     case head :: tail => sum(head + acc, tail)
   }
 
@@ -167,21 +194,19 @@ object Main extends App {
   def sumTailRec(xs: List[Int]): Int = {
     @tailrec
     def sumInternal(xs: List[Int], acc: Int): Int = xs match {
-      case Nil => 0
+      case Nil          => 0
       case head :: tail => sumInternal(tail, head + acc)
     }
 
     sumInternal(xs, 0)
   }
 
-
   def fibonacciList(upTo: Int): List[Int] = {
     @tailrec
     def fibInternal(upTo: Int, acc: List[Int]): List[Int] = {
       if (upTo == 0) {
         acc
-      }
-      else {
+      } else {
         acc match {
           case x :: y :: xs => fibInternal(upTo - 1, (x + y) :: acc)
         }
@@ -196,8 +221,7 @@ object Main extends App {
     def fibInternal(upTo: Int, cur: Int, prev: Int): Int = {
       if (upTo == 0) {
         cur
-      }
-      else {
+      } else {
         fibInternal(upTo - 1, cur + prev, cur)
       }
     }
@@ -211,7 +235,9 @@ object Main extends App {
     sealed trait GameAction
     object GameAction {
       case object Quit extends GameAction
+
       case object Continue extends GameAction
+
       case object NewGame extends GameAction
     }
 
@@ -226,7 +252,13 @@ object Main extends App {
       val (nextState, nextAction) = input match {
         case "H" | "T" =>
           val matched = flip == input
-          (cur.copy(numFlips = cur.numFlips + 1, correctFlips = cur.correctFlips + (if (matched) 1 else 0)), GameAction.Continue)
+          (
+            cur.copy(
+              numFlips = cur.numFlips + 1,
+              correctFlips = cur.correctFlips + (if (matched) 1 else 0)
+            ),
+            GameAction.Continue
+          )
         case "Q" =>
           println(cur)
           (cur, GameAction.Quit)
@@ -237,9 +269,9 @@ object Main extends App {
           (cur, GameAction.Continue)
       }
       nextAction match {
-        case GameAction.Quit =>
+        case GameAction.Quit     =>
         case GameAction.Continue => main(nextState, pastGames)
-        case GameAction.NewGame => main(nextState, pastGames :+ cur)
+        case GameAction.NewGame  => main(nextState, pastGames :+ cur)
       }
     }
 
@@ -248,24 +280,22 @@ object Main extends App {
 
   // coinTossGame()
 
-
   // Codility challenge MissingInteger - find minimum positive number >0 not in the input
   object Solution {
     def solution(a: List[Int]): Int = {
       @tailrec
       def internal(remaining: List[Int], acc: Set[Int]): Int = remaining match {
-        case Nil => acc.min
+        case Nil     => acc.min
         case x :: xs => internal(xs, acc - x)
       }
 
-      internal(a.toList,Range(1, 100_000).toSet)
+      internal(a.toList, Range(1, 100_000).toSet)
     }
 
     assert(5 == Solution.solution(List(1, 3, 6, 4, 1, 2)))
     assert(4 == Solution.solution(List(1, 2, 3)))
     assert(1 == Solution.solution(List(-1, -3)))
   }
-
 
   // For-comprehensions.
   // Get converted by compiler into series of map, flatMap, foreach, withFilter.  It's just sugar.
@@ -277,11 +307,10 @@ object Main extends App {
   )
 
   val namesStartingWithB: List[String] = for {
-    p <- people                 // generator
-    fname = p.firstName         // definition
-    if (fname startsWith "b")   // filter
+    p <- people // generator
+    fname = p.firstName // definition
+    if (fname startsWith "b") // filter
   } yield fname
-
 
   // What data types can be used in for-comprehensions?
   // If it has .map(), can be used in for comprehensions containing a single generator.
@@ -292,31 +321,31 @@ object Main extends App {
 
   abstract class CustomClass[A] {
     def map[B](f: A => B): CustomClass[B]
+
     def flatMap[B](f: A => CustomClass[B]): CustomClass[B]
+
     def withFilter(p: A => Boolean): CustomClass[A]
+
     def foreach(b: A => Unit): Unit
   }
-
 
   case class Sequence[A](initialElems: A*) {
     private val elems = scala.collection.mutable.ArrayBuffer[A]()
     elems ++= initialElems
 
     def foreach(f: A => Unit): Unit = elems.foreach(f)
-//    def map[B](f: A => B): Sequence[B] = Sequence(elems.map(f))
-//    def withFilter(f: A => Boolean): Sequence[A] = Sequence(elems.withFilter(f))
-//    def flatMap[B](f: A => Sequence[B]): Sequence[B] = {
-//      val mapped: Sequence[Sequence[B]] = map(f)
-//      // flatten not written but if it were would now do:
-//      // flatten(mapped)
-//      ???
-//    }
+    //    def map[B](f: A => B): Sequence[B] = Sequence(elems.map(f))
+    //    def withFilter(f: A => Boolean): Sequence[A] = Sequence(elems.withFilter(f))
+    //    def flatMap[B](f: A => Sequence[B]): Sequence[B] = {
+    //      val mapped: Sequence[Sequence[B]] = map(f)
+    //      // flatten not written but if it were would now do:
+    //      // flatten(mapped)
+    //      ???
+    //    }
   }
-
 
   // Understanding flatMap.
   // On sequences, works much like map + flatten.  (Flatten takes a list of lists, and produces a flattened list of elements.)
-
 
   // Either.
   // Used to have no preference between Left and Right, it was just convention to have Left=error, Right=success.
@@ -325,21 +354,24 @@ object Main extends App {
 
   // He prefers Option and Try for error handling, over Either.  Of course, don't throw!
 
-
   // Advanced blocks:
-  def takesInt(input: => Int): Unit = ???
-  def takesFunction(input: Int => Int): Unit = ???
+  def takesInt(input: => Int): Unit = println("Got int")
+
+  def takesFunction(input: Int => Int): Unit = println("Got func")
 
   takesInt {
     42
   }
 
-  takesFunction { a => a * 2 }
-
+  takesFunction { a =>
+    a * 2
+  }
 
   // Bind.
   def f(a: Int): (Int, String) = ???
+
   def g(a: Int): (Int, String) = ???
+
   def h(a: Int): (Int, String) = ???
 
   def bind(f: Int => (Int, String), result: (Int, String)): (Int, String) = {
@@ -347,13 +379,15 @@ object Main extends App {
     (next._1, next._2 + result._2)
   }
 
-  val fResult = f(100)
-  val gResult = bind(g, fResult)
-  val hResult = bind(h, gResult)
-
+  def bindTest() {
+    val fResult = f(100)
+    val gResult = bind(g, fResult)
+    val hResult = bind(h, gResult)
+  }
 
   case class Wrapper[T](value: T) {
     def map[Y](f: T => Y): Wrapper[Y] = Wrapper(f(value))
+
     def flatMap[Y](f: T => Wrapper[Y]): Wrapper[Y] = f(value)
   }
 
@@ -369,8 +403,117 @@ object Main extends App {
     def flatMap[Y](f: T => Debuggable[Y]): Debuggable[Y] = {
       val next = f(value)
       Debuggable(next.value, next.message + " " + message)
+    }
   }
 
+  // State monads.
+  case class GolfState(distance: Int)
+
+  def swing(distance: Int, dbg: String): State[GolfState, Int] = State { (s: GolfState) =>
+    val newAmount = s.distance + distance
+    println(s"${dbg}: dist=${distance} new=${newAmount}")
+    (GolfState(newAmount), newAmount)
+  }
+
+  println("State buildtime")
+
+  // The point of a State monad is to allow writing code like this:
+  val stateWithNewDistance: State[GolfState, Int] = for {
+    _             <- swing(20, "A")
+    _             <- swing(15, "B")
+    totalDistance <- swing(0, "C")
+  } yield totalDistance
+
+  val beginningState = GolfState(0)
+  println("State runtime")
+  val finalState: (GolfState, Int) = stateWithNewDistance.run(beginningState) // will be 35
+
+  case class State[S, A](run: S => (S, A)) {
+    def flatMap[B](g: A => State[S, B]): State[S, B] = State { (s0: S) =>
+      println("flatMap")
+      val (s1, a) = run(s0)
+      val next = g(a)
+      next.run(s1)
+    }
+
+    def map[B](f: A => B): State[S, B] = flatMap(a => State.point(f(a)))
+  }
+
+  object State {
+    def point[S, A](v: A): State[S, A] = State(run = s => (s, v))
+  }
+
+  // This is complex, trying to walk it through:
+  // GolfState(0) simple enough.
+  // stateWithNewDistance returns a State.
+  // A State is a case class with single field - a run function.
+  // I guess calling run() evaluates the full for-comprehension.  But how.
+  // Each run() is going to ultimately call our swing method, which calculates our new state.
+  // So it's a bit like reactive, with build time vs execution time.
+  // The for-comprehension is building 3 States via State.flatMap, which (maybe) recursively know about each other.
+  // Executing the States calls the run() functions, which point at swing().
+  //
+  // So during the for-comprehension (build time):
+  // * swing(20) is called.  Immediately returns a State with a run() that will later:
+  //   Take a state s.  Add distance 20 to s.distance, and spit out a GolfState containing that new distance.
+  // * Now swing(15) is called.  Does same.
+  // * And same again for swing(0).
+  // Ok so conceptually alright so far: each State is going to, at "runtime", take a previous GolfState, apply a change
+  // to it, return an update GolfState.
+  //
+  // So mechanically how do things actually work at runtime:
+  // * Call stateWithNewDistance.run(beginningState)
+  // * Somehow we're in State.flatMap.  s0 is beginningState - GolfState(0).
+  // * So run is pointing at flatMap?  Unclear.
+  // * Call run(s0)
+  // * This executes swing(20).
+  // * Ok so we must be executing the State that swing(20) returned.
+  // * Returns a GolfState(20) to flatMap.
+  // * flatMap has been passed a method g, that's going to turn a GolfState into a State.
+  // * We call next = g(20).
+  // * This goes back into the for-comprehension.  Unclear.
+  // * next.run(GolfState(20)).
+  // * Back into State.flatMap.
+  // * Ok, don't totally get what's happening mechanically, but get the broad strokes.
+  // * Can see that both flatMap and swing are only called at runtime.
+  //
+  // This could help: https://www.reddit.com/r/scala/comments/ehkzrq/state_monad_learn_how_it_works/
+  //
+  // Something the book doesn't do a good job on - _why_ I'd want to write code like this, instead of a simpler
+  // procedural style.  It's deferring all state manipulation until runtime (impure core) - but at quite a cost of
+  // complexity.
+
+
+  // Mixing Monads in a for-comprehension is annoyingly impossible.  Can't have Trys mixed with Options.
+  // Because under the hood it's a bunch of flatMap calls, so the types have to match.
+  // Solutions include Free monads, monad zippers and views, monad coproducts, monad transformers.
+
+  // Monad transformers.
+  // Generally end in T - OptionT,EitherT,StateT (all in Cats).
+
+  // First: lift/point/pure (different names, same concept).
+  // "Lifting" a simple value into a Monad.  `Some(1)` is lifting 1 into an Option.
+  //
+  // Ok a huge section now that I'll have to come back to, about Monad transformers.  Results in code that allows
+  // IO input and passes state around this big for expression that recursively calls itself, and is all effect driven
+  // so it only evaluates at runtime:
+
+//  def sumLoop: StateT[IO, SumState, Unit] = for {
+//    _ <- putStrAsStateT("\ngive me an int, or 'q' to quit: ")
+//    input <- getLineAsStateT
+//    _ <- if (input == "q") {
+//      liftIoIntoStateT(IO(Unit))
+//    } else for {
+//      i <- liftIoIntoStateT(IO(toInt(input)))
+//      _ <- doSumWithStateT(i)
+//      _ <- sumLoop
+//    } yield Unit
+//  } yield Unit
+//  val result = sumLoop.run(SumState(0)).run
+//  println(s"Final SumState: ${result}")
+
+
+  // Cats
 
 
 }
